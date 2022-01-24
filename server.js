2dbdf7d2ee37f4222f3e4e6221f4d1278c6370cd
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-01-23 23:36:09 -0900

    create user variable inside server.js for storing users

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// This is bad practice, I should use a database to store the user information, but I'll use it for testing purposes.
+const users = [];
+
 // This tells the server we're using ejs syntax.
 app.set('view-engine', 'ejs');
 //  This tells the application that we want to take the forms we have in the .ejs files and we want to be able to access them inside the "req" variable in the post routes.
